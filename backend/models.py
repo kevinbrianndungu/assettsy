4d9d32125cc775e924ac0b99ca3a3057483bc5ef
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-05-05 17:10:05 +0300

    models

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,18 @@
 from . import db
 from flask_login import UserMixin
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(200))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
+class Asset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    condition = db.Column(db.String(50))
+    department = db.Column(db.String(100))
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
