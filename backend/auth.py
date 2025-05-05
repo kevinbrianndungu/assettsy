d4c54dfa8adc09bb16c93a940503930a1a05648e
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-05-05 19:11:03 +0300

    auth

--- backend/auth.py
+++ backend/auth.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
-from backend.models import User
-from backend import db
+from backend.models import db, User
 
 auth = Blueprint('auth', __name__)
 
@@ -12,13 +11,10 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
-
         if user and check_password_hash(user.password, password):
-            login_user(user, remember=True)
+            login_user(user)
             return redirect(url_for('views.dashboard'))
-        else:
-            flash('Invalid email or password', 'danger')
-    
+        flash('Invalid login credentials.')
     return render_template('login.html')
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -27,21 +23,11 @@ def signup():
         email = request.form.get('email')
         name = request.form.get('name')
         password = request.form.get('password')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already registered.', 'warning')
-        else:
-            new_user = User(
-                email=email,
-                name=name,
-                password=generate_password_hash(password, method='sha256')
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            return redirect(url_for('views.dashboard'))
-
+        hashed_password = generate_password_hash(password)
+        user = User(email=email, name=name, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
     return render_template('signup.html')
 
 @auth.route('/logout')
@@ -49,5 +35,4 @@ def signup():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
 
