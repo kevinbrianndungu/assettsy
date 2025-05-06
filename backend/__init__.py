51c9e3c488bc4b3b0847d8a9a543624e4c8bb8cc
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-05-06 16:11:24 +0300

    innit

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,27 +1,39 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config['SECRET_KEY'] = 'your-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
 
-    # Import and register blueprints if any
-    from .views import views
-    from .auth import auth
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    # Ensure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    from .models import User
+    from backend.models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from backend.auth import auth as auth_blueprint
+    from backend.views import views as views_blueprint
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(views_blueprint)
 
     return app
 
