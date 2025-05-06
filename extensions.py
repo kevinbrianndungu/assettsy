eafcad2f94d6e47c0d524f88e48f85ba06524cc8
kevinbrianndungu <kevinbrianndungu@gmail.com>
2025-05-06 16:52:21 +0300

    extensions

---
+++ extensions.py
@@ -0,0 +1,12 @@
+# backend/extensions.py
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+# Create instances of extensions
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+migrate = Migrate()
+
